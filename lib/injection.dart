4b30d1d18bc5b0117730bd701e8753ee20e2e0c9
Muhammad Dadang Saputra <muhdadangs@gmail.com>
2023-06-27 20:14:29 +0700

    initialize use cases on injection.dart

--- lib/injection.dart
+++ lib/injection.dart
@@ -4,11 +4,17 @@ import 'package:http/http.dart' as http;
 import 'data/datasources/sikomik_remote_data_source.dart';
 import 'data/repositories/sikomik_repository_impl.dart';
 import 'domain/repositories/sikomik_repository.dart';
+import 'domain/usecases/get_chapter_case.dart';
+import 'domain/usecases/get_latest_manga_case.dart';
+import 'domain/usecases/get_manga_detail_case.dart';
 
 final locator = GetIt.instance;
 
 void init() {
   // use case
+  locator.registerLazySingleton(() => GetLatestMangaCase(repository: locator()));
+  locator.registerLazySingleton(() => GetMangaDetailCase(repository: locator()));
+  locator.registerLazySingleton(() => GetChapterCase(repository: locator()));
 
   // repository
   locator.registerLazySingleton<SiKomikRepository>(
