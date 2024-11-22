8120e1a57e3d5e5c6350a9228e2724b0f5f4e195
Muhammad Dadang Saputra <muhdadangs@gmail.com>
2024-11-23 00:05:17 +0700

    implement favorites comic page

---
+++ lib/presentation/controllers/favorites_controller.dart
@@ -0,0 +1,44 @@
+import 'package:get/get.dart';
+
+import '../../common/enums.dart';
+import '../../domain/entities/user_comic_entity.dart';
+import '../../domain/usecases/user_comic_case.dart';
+import '../../injection.dart';
+import 'main_controller.dart';
+
+class FavoritesController extends GetxController {
+  final UserComicCase userComicCase = locator();
+
+  final MainController mainController = Get.find<MainController>();
+
+  Rx<RequestState> stateFavorites = RequestState.loading.obs;
+
+  RxList<UserComicEntity> favorites = <UserComicEntity>[].obs;
+
+  void initialize() {
+    getFavorites();
+  }
+
+  Future<void> getFavorites() async {
+    changeStateFavorites(RequestState.loading);
+    if (mainController.user.value == null) {
+      changeStateFavorites(RequestState.error);
+    }
+
+    final result = await userComicCase.getFavorites(
+      userId: mainController.user.value?.uid ?? "",
+    );
+
+    result.fold((l) {
+      changeStateFavorites(RequestState.error);
+    }, (r) {
+      favorites.assignAll(r.where((item) => item.comic != null));
+      changeStateFavorites(RequestState.loaded);
+    });
+  }
+
+  void changeStateFavorites(RequestState value) {
+    stateFavorites.value = value;
+    stateFavorites.refresh();
+  }
+}
---
+++ lib/presentation/pages/favorites/responsives/favorites_page_phone.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../../common/enums.dart';
+import '../../../../common/theme.dart';
+import '../../../controllers/favorites_controller.dart';
+import '../../../widgets/comic_favorite_card_widget.dart';
+
+class FavoritesPagePhone extends StatelessWidget {
+  const FavoritesPagePhone({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: bgColor,
+      appBar: AppBar(
+        title: Text(
+          "Komik Favorite",
+          style: theme.textTheme.titleLarge?.copyWith(
+            color: theme.primaryColor,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: GetX<FavoritesController>(
+        builder: (controller) => RefreshIndicator(
+          onRefresh: controller.getFavorites,
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 24.0,
+                horizontal: 16.0,
+              ),
+              child: Column(
+                children: [
+                  if (controller.favorites.isEmpty)
+                    Center(
+                      child: Text(
+                        "Anda belum menambahkan komik ke daftar favorite",
+                        textAlign: TextAlign.center,
+                        style: theme.textTheme.labelLarge?.copyWith(
+                          color: Colors.white,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ),
+                  if (controller.favorites.isNotEmpty)
+                    GridView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: (size.width / 250).round(),
+                        crossAxisSpacing: 8.0,
+                        mainAxisSpacing: 8.0,
+                        mainAxisExtent: 260,
+                      ),
+                      itemCount: controller.favorites.length,
+                      itemBuilder: (context, index) {
+                        final item = controller.favorites[index];
+
+                        return ComicFavoriteCardWidget(userComic: item);
+                      },
+                    ),
+                  if (controller.stateFavorites.value == RequestState.loading)
+                    const SizedBox(
+                      height: 100,
+                      child: Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
