af9ad3a29e9ba76170c78c98a4f2c9104d8bf941
Muhammad Dadang Saputra <muhdadangs@gmail.com>
2025-05-27 07:46:41 +0700

    implementation of repository unit test

---
+++ test/data/datasources/sikomik_firebase_firestore_data_source_test.dart
@@ -0,0 +1,5 @@
+import 'package:SiKomik/data/datasources/sikomik_firebase_firestore_data_source.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks([SiKomikFirebaseFirestoreDataSource])
+void main() {}
