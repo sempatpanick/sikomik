021e3bd264690933842e1a15391b76a7f162517f
Muhammad Dadang Saputra <muhdadangs@gmail.com>
2025-06-07 21:52:15 +0700

    give padding on chapter detail, so it will see as popup well

--- lib/presentation/pages/chapter/widgets/chapter_detail_popup_widget.dart
+++ lib/presentation/pages/chapter/widgets/chapter_detail_popup_widget.dart
@@ -9,38 +9,22 @@ class ChapterDetailPopupWidget extends StatelessWidget {
     final theme = Theme.of(context);
     final navigator = Navigator.of(context);
 
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        SingleChildScrollView(
-          child: InteractiveViewer(
-            child: Image(
-              image: NetworkImage(
-                item,
-                webHtmlElementStrategy: WebHtmlElementStrategy.fallback,
-              ),
-              fit: BoxFit.fill,
-              frameBuilder: (context, child, value, state) {
-                if (value == null) {
-                  return SizedBox(
-                    width: double.infinity,
-                    height: 100,
-                    child: Center(
-                      child: SizedBox(
-                        width: 40,
-                        height: 40,
-                        child: CircularProgressIndicator(
-                          color: theme.primaryColor,
-                        ),
-                      ),
-                    ),
-                  );
-                }
-                return child;
-              },
-              loadingBuilder: (context, child, event) => event == null
-                  ? child
-                  : SizedBox(
+    return Padding(
+      padding: EdgeInsets.all(32.0),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          SingleChildScrollView(
+            child: InteractiveViewer(
+              child: Image(
+                image: NetworkImage(
+                  item,
+                  webHtmlElementStrategy: WebHtmlElementStrategy.fallback,
+                ),
+                fit: BoxFit.fill,
+                frameBuilder: (context, child, value, state) {
+                  if (value == null) {
+                    return SizedBox(
                       width: double.infinity,
                       height: 100,
                       child: Center(
@@ -48,41 +32,60 @@ class ChapterDetailPopupWidget extends StatelessWidget {
                           width: 40,
                           height: 40,
                           child: CircularProgressIndicator(
-                            value: event.cumulativeBytesLoaded /
-                                (event.expectedTotalBytes ?? 1),
                             color: theme.primaryColor,
                           ),
                         ),
                       ),
+                    );
+                  }
+                  return child;
+                },
+                loadingBuilder: (context, child, event) => event == null
+                    ? child
+                    : SizedBox(
+                        width: double.infinity,
+                        height: 100,
+                        child: Center(
+                          child: SizedBox(
+                            width: 40,
+                            height: 40,
+                            child: CircularProgressIndicator(
+                              value: event.cumulativeBytesLoaded /
+                                  (event.expectedTotalBytes ?? 1),
+                              color: theme.primaryColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                errorBuilder: (context, url, error) => const SizedBox(
+                  width: double.infinity,
+                  height: 100,
+                  child: Center(
+                    child: Icon(
+                      Icons.broken_image_outlined,
+                      color: Colors.grey,
+                      size: 40,
                     ),
-              errorBuilder: (context, url, error) => const SizedBox(
-                width: double.infinity,
-                height: 100,
-                child: Center(
-                  child: Icon(
-                    Icons.broken_image_outlined,
-                    color: Colors.grey,
-                    size: 40,
                   ),
                 ),
               ),
             ),
           ),
-        ),
-        Align(
-          alignment: Alignment.topRight,
-          child: IconButton(
-            onPressed: navigator.pop,
-            icon: CircleAvatar(
-              backgroundColor: Colors.white,
-              child: Icon(
-                Icons.close,
-                color: theme.primaryColor,
+          Align(
+            alignment: Alignment.topRight,
+            child: IconButton(
+              onPressed: navigator.pop,
+              icon: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Icon(
+                  Icons.close,
+                  color: theme.primaryColor,
+                ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
